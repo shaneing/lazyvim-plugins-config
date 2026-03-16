04ef168ae3630f4b1e6b5da411f212c29547e87e
Shane <im.xianying@gmail.com>
2026-03-16 14:13:33 +0800

    update go plugin

--- go.lua
+++ go.lua
@@ -22,7 +22,7 @@ return {
     optional = true,
     dependencies = {
       {
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
           vim.list_extend(opts.ensure_installed, { "delve" })
